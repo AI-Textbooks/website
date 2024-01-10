7a3ac78c14d5cf7e5070fdf52063efbba58838a1
mattorp <matto@oeye.site>
2024-01-10 13:31:46 +0100

    fix list styling not applying in markdown

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,52 +1,59 @@
-import type { Metadata } from "next";
-import { Hind } from "next/font/google";
-import { AppBar } from "./AppBar";
-import "./globals.css";
-import Footer from "./components/Footer";
+import type { Metadata } from 'next'
+import { Hind } from 'next/font/google'
+import { AppBar } from './AppBar'
+import Footer from './components/Footer'
+import './globals.css'
 
-const inter = Hind({ subsets: ["latin"], weight: ["300"] });
+const inter = Hind({ subsets: ['latin'], weight: ['300'] })
 
 export const metadata: Metadata = {
-  title: "AI Textbooks",
-  description: "The global knowledge base architecture",
+  title: 'AI Textbooks',
+  description: 'The global knowledge base architecture',
   metadataBase: new URL('https://ai-textbooks.com'),
   openGraph: {
-    title: "AI Textbooks",
-    images: '/api/og'
+    title: 'AI Textbooks',
+    images: '/api/og',
   },
 }
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
         <meta
-          name="description"
-          content="Nurturing a future where AI models become interfaces between open knowledge repositories."
+          name='description'
+          content='Nurturing a future where AI models become interfaces between open knowledge repositories.'
         />
-        <link rel="canonical" href="https://ai-textbooks.com" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel='canonical' href='https://ai-textbooks.com' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
 
         {/*
         	Open graph meta tags.
     	*/}
-        <meta property="og:locale" content="en_US" />
-        <meta property="og:site_name" content="AI Textbooks" />
+        <meta property='og:locale' content='en_US' />
+        <meta property='og:site_name' content='AI Textbooks' />
         <meta
-          property="og:description"
-          content="Nurturing a future where AI models become interfaces between open knowledge repositories."
+          property='og:description'
+          content='Nurturing a future where AI models become interfaces between open knowledge repositories.'
         />
-        <meta property="og:url" content="https://ai-textbooks.com" />
+        <meta property='og:url' content='https://ai-textbooks.com' />
       </head>
       <body className={inter.className}>
+        {/* Empty lists to enforce tailwind compiling it for use in markdown */}
+        <ul className='hidden'>
+          <li className='list-disc list-inside' />
+        </ul>
+        <ol className='hidden'>
+          <li className='list-decimal list-inside' />
+        </ol>
         <AppBar />
         {children}
         <Footer />
       </body>
     </html>
-  );
+  )
 }
