04111449976e609c01761f942139ab6271ee3db2
John Choque <johnchque.tech@gmail.com>
2024-01-11 15:02:38 +0100

    Fixing benefits.

--- app/components/Benefits.tsx
+++ app/components/Benefits.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import React from "react";
 import Container from "./container";
 
@@ -41,26 +40,7 @@ const Benefits = () => {
   );
 }
 
-function Avatar(props) {
-  return (
-    <div className="flex items-center mt-8 space-x-3">
-      <div className="flex-shrink-0 overflow-hidden rounded-full w-14 h-14">
-        <Image
-          src={props.image}
-          width="40"
-          height="40"
-          alt="Avatar"
-        />
-      </div>
-      <div>
-        <div className="text-lg font-medium">{props.name}</div>
-        <div className="text-gray-600 dark:text-gray-400">{props.title}</div>
-      </div>
-    </div>
-  );
-}
-
-function Mark(props) {
+function Mark(props: { children: string }) {
   return (
     <>
       {" "}
